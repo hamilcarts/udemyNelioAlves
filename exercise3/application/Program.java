540cefa58e7a06e1aa8a613921e15bdba5072663
Hamilcar Tortora da Silva <109037178+hamilcarts@users.noreply.github.com>
2023-01-03 22:16:57 -0300

    Commit exercise 3

--- exercise3/application/Program.java
+++ exercise3/application/Program.java
@@ -1,4 +1,20 @@
 package exercise3.application;
 
+import exercise3.entities.Student;
+
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Student student = new Student();
+        System.out.println("Enter the student's name:");
+        student.setNome(scan.nextLine());
+        System.out.println("Enter the three notes:");
+        student.setNote1(scan.nextDouble());
+        student.setNote2(scan.nextDouble());
+        student.setNote3(scan.nextDouble());
+        student.finalNote();
+        scan.close();
+    }
 }
--- exercise3/entities/Student.java
+++ exercise3/entities/Student.java
@@ -1,4 +1,57 @@
 package exercise3.entities;
 
 public class Student {
+    private String nome;
+    private double note1;
+    private double note2;
+    private double note3;
+
+    public void finalNote() {
+        double average = note1 + note2 + note3;
+        if (average > 60) {
+            System.out.printf("FINAL GRADE: %.2f", average);
+            System.out.println();
+            System.out.println("PASS");
+        } else {
+            System.out.printf("FINAL GRADE: %.2f", average);
+            System.out.println();
+            System.out.println("FAILED");
+            System.out.printf("MISSING %.2f POINTS", (60 - average));
+        }
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getNote1() {
+        return note1;
+    }
+
+    public void setNote1(double note1) {
+        this.note1 = note1;
+    }
+
+    public double getNote2() {
+        return note2;
+    }
+
+    public void setNote2(double note2) {
+        this.note2 = note2;
+    }
+
+    public double getNote3() {
+        return note3;
+    }
+
+    public void setNote3(double note3) {
+        this.note3 = note3;
+    }
+
+
 }
